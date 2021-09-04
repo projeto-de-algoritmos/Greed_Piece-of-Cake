ee6398b9a11b71cc2a3094f0dc74348313130977
Lucas Gabriel <lucasbsb100@gmail.com>
2021-09-04 01:07:53 -0300

    Commit inicial
    Co-authored-by: Hugo Bezerra  <hugoricardo.so.be@gmail.com>

---
+++ piece_of_cake/android/app/src/main/kotlin/com/example/piece_of_cake/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.piece_of_cake
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ piece_of_cake/lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Piece of Cake',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Piece of Cake'),
+          centerTitle: true,
+        ),
+        body: Container(
+        ),
+      )
+    );
+  }
+}
+
